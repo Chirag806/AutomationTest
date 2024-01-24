f71ca0b3263be9e1af65331452ff369a97ed6fc7
Chirag806 <cranoliya806@gmail.com>
2024-01-24 15:46:01 +0530

    Commit 1

--- AutomateLogin/src/Test1.java
+++ AutomateLogin/src/Test1.java
@@ -1,4 +1,10 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Test1 {
+	public static void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver","C:\\Users\\Chirag\\Documents\\geckodriver.exe");
+	WebDriver driver= new FirefoxDriver();
+	}
 
 }
